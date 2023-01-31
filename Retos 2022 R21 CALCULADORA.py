d1ba5c3486562d8c614508ac3f3978bf0e42e493
Julio Betancourt <juliobetancourt@MacBook-Pro-de-Julio.local>
2023-01-31 00:25:47 -0600

    Reto 21

--- Retos 2022 R21 CALCULADORA.py
+++ Retos 2022 R21 CALCULADORA.py
@@ -12,46 +12,56 @@
 #  * - Soporta las operaciones suma "+", resta "-", multiplicación "*" y división "/".
 #  * - El resultado se muestra al finalizar la lectura de la última línea (si el .txt es correcto).
 #  * - Si el formato del .txt no es correcto, se indicará que no se han podido resolver las operaciones.
-#  * 
+#  */
 
 def main():
     operation = ""
     ban1 = 0
-    ban2 = 0 
     op1 = 0
     op2 = 0 
-    result = "Error"
+    symbol = ""
+    result = 0
+    res = ""
     with open("Challenge21.txt", "r") as file:
         content = file.readlines()
 
+    # 5 + 10.1 - 2 * 3 / 1
+    content = [x.strip() for x in content]
+
     for line in content:
         operation += line
-        if not line.isnumeric():
-            if (ban1 == 0):
-                result = 'Error'
-                break
-            else:
-                if line == "+":
-                    result = op1 + op2 
-                elif line == "-":
-                    result = op1 - op2 
-                elif line == "*":
-                    result = op1 * op2
-                elif line == "/":
-                    result = op1 / op2
-                else:
-                    result = "Err"
-                    break
-            op1 = result
-        else:
+        if  line.isnumeric() or line.replace('.','').isdigit():
             if ban1 == 0:
                 op1 = float(line)
                 ban1 = 1
             else:
                 op2 = float(line)
+                if symbol == "+":
+                    result = op1 + op2 
+                elif symbol == "-":
+                    result = op1 - op2 
+                elif symbol == "*":
+                    result = op1 * op2
+                elif symbol == "/":
+                    result = op1 / op2
+                else:
+                    res = "Error"
+                    break
+                op1 = result    
+            
+        else:
+            if (ban1 == 0):
+                res = 'Error'
+                break
+            
+            if line == "+" or line == "-" or line == "*" or line == "/":
+                symbol = line
+            else:
+                res = "Error"
+                break
 
     
-    print("operacion", " ".join(x for x in operation.split()), " = ", result)
+    print("operacion", " ".join(x for x in operation.split()), " = ", "Error" if res == "Error" else result)
 
 
 if __name__ == "__main__":
