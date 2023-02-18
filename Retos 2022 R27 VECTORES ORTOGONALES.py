96bd45c30905a82c605a2d95b75ae178aeb739e7
Julio <juliobetancourt@MacBook-Pro-de-Julio.local>
2023-02-18 14:45:46 -0600

    R27

---
+++ Retos 2022 R27 VECTORES ORTOGONALES.py
@@ -0,0 +1,12 @@
+# /*
+#  * Reto #27
+#  * VECTORES ORTOGONALES
+#  * Fecha publicación enunciado: 07/07/22
+#  * Fecha publicación resolución: 11/07/22
+#  * Dificultad: FÁCIL
+#  *
+#  * Enunciado: Crea un programa que determine si dos vectores son ortogonales.
+#  * - Los dos array deben tener la misma longitud.
+#  * - Cada vector se podría representar como un array. Ejemplo: [1, -2]
+#  */
+
